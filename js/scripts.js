c69456cce1a57e462bdfab53f8d40790cbb24d09
Matt Staudacher <mattstaudacher3@gmail.com>
2018-06-12 14:32:48 -0700

    added constructor and prototype to js for movie choice.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,41 @@
+function Movie(title, time, adult, child, senior) {
+  this.title = title;
+  this.time = time;
+  this.adult = adult;
+  this.child = child;
+  this.senior= senior;
+}
+
+Movie.prototype.total = function() {
+
+  return adultPrice*inputAdultTickets+childPrice*inputChildTickets+seniorPrice*inputSeniorTickets;
+}
+
+//userI
+
 $(document).ready(function() {
   $("#new-movie").submit(function(event) {
       event.preventDefault();
 
+      Movie.prototype.total = function() {
+
+        return adultPrice*inputAdultTickets+childPrice*inputChildTickets+seniorPrice*inputSeniorTickets;
+      }
+
       var inputAdultTickets = $("input#adult-tickets").val();
       var inputChildTickets = $("input#child-tickets").val();
       var inputSeniorTickets = $("input#senior-tickets").val();
+      var selectedTime = $("input:radio[name=times]:checked").val();
+      var selectedMovie = $("select#movie-titles").val();
+      var total= "";
 
-      var inputAdultTickets = 12;
-      var inputChildTickets = 7;
-      var inputSeniorTickets= 5;
+      var newMovie = new Movie (selectedMovie, selectedTime,inputAdultTickets,inputChildTickets,inputSeniorTickets)
 
-      var total=
+      var adultPrice = 12;
+      var childPrice = 7;
+      var seniorPrice = 5;
+
+
 
 
 
@@ -27,7 +52,7 @@ $(document).ready(function() {
           $(".number-of-ticket").text("");
           $(".movie-cost").text(newContact.lastName);
 
-          
+
       $(".new-address").each(function() {
         var inputtedStreet = $(this).find("input.new-street").val();
         var inputtedCity = $(this).find("input.new-city").val();
